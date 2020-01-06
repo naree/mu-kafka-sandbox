46fd0a7b775c2eaf21a9d16f4696dad8bd0a5fed
Naree Song <naree.song@47deg.com>
2020-01-06 12:07:21 +0000

    uncomment the code and fix to make it compile

--- src/main/scala/foo/main/Producer.scala
+++ src/main/scala/foo/main/Producer.scala
@@ -9,9 +9,17 @@ import fs2.Stream
 object Producer extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
     val messageQueue: Stream[IO, Queue[IO, Option[UserWithCountry]]] = Stream.eval(Queue.bounded[IO, Option[UserWithCountry]](1))
-    val users: Stream[IO, UserWithCountry] = Stream(UserWithCountry("naree", 1, "singapore"))
+    val users: Stream[IO, Option[UserWithCountry]] = Stream(Some(UserWithCountry("naree", 1, "singapore")))
 
-//    foo.kafka.Producer[IO, UserWithCountry](broker, topic, messageQueue, users).unsafeRunSync()
+    val producer = for {
+      queue <- messageQueue
+      result <- Stream(
+        users.through(queue.enqueue),
+        foo.kafka.Producer.streamWithQueue(broker, topic, queue))
+        .parJoin(2)
+    } yield result
+
+    producer.compile.drain.unsafeRunSync()
     IO(ExitCode.Success)
   }
 }
--- src/main/scala/foo/main/Producer2.scala
+++ src/main/scala/foo/main/Producer2.scala
@@ -9,9 +9,18 @@ import fs2.concurrent.Queue
 object Producer2 extends IOApp {
   override def run(args: List[String]): IO[ExitCode] = {
     val messageQueue: Stream[IO, Queue[IO, Option[UserWithRegion]]] = Stream.eval(Queue.bounded[IO, Option[UserWithRegion]](1))
-    val users: Stream[IO, UserWithRegion] = Stream(UserWithRegion("naree", 1, Some("apac")))
+    val users: Stream[IO, Option[UserWithRegion]] = Stream(Some(UserWithRegion("naree", 1, Some("apac"))))
 
-//    foo.kafka.Producer(broker, topic, messageQueue, users).unsafeRunSync()
+    val producer = for {
+      queue <- messageQueue
+      result <- Stream(
+        users.through(queue.enqueue),
+        foo.kafka.Producer.streamWithQueue(broker, topic, queue))
+        .parJoin(2)
+    } yield result
+
+    producer.compile.drain.unsafeRunSync()
+
     IO(ExitCode.Success)
   }
 }
