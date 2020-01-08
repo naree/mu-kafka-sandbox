4d0cf4cd823206cfeb97e8de0b70cd88eeb81237
Naree Song <naree.song@47deg.com>
2020-01-08 14:37:31 +0000

    Format cleaning up

--- src/main/scala/foo/kafka/Consumer.scala
+++ src/main/scala/foo/kafka/Consumer.scala
@@ -15,26 +15,26 @@ object Consumer {
 
   implicit def unsafeLogger[F[_] : Sync] = Slf4jLogger.getLogger[F]
 
-    def settings[F[_]](groupId: String, broker: String)(implicit sync: Sync[F]): ConsumerSettings[F, String, Array[Byte]] =
-      ConsumerSettings[F, String, Array[Byte]]
-        .withGroupId(groupId)
-        .withBootstrapServers(broker)
-        .withAutoOffsetReset(AutoOffsetReset.Latest)
+  def settings[F[_]](groupId: String, broker: String)(implicit sync: Sync[F]): ConsumerSettings[F, String, Array[Byte]] =
+    ConsumerSettings[F, String, Array[Byte]]
+      .withGroupId(groupId)
+      .withBootstrapServers(broker)
+      .withAutoOffsetReset(AutoOffsetReset.Latest)
 
-    def apply[F[_], A: SchemaFor : FromRecord](groupId: String, topic: String, broker: String)
-                                              (implicit contextShift: ContextShift[F],
-                                               concurrentEffect: ConcurrentEffect[F], timer: Timer[F]): F[Unit] =
-      stream(groupId, topic, broker).compile.drain
+  def apply[F[_], A: SchemaFor : FromRecord](groupId: String, topic: String, broker: String)
+                                            (implicit contextShift: ContextShift[F],
+                                             concurrentEffect: ConcurrentEffect[F], timer: Timer[F]): F[Unit] =
+    stream(groupId, topic, broker).compile.drain
 
-    def stream[F[_], A: SchemaFor : FromRecord](groupId: String, topic: String, broker: String)
-                                               (implicit contextShift: ContextShift[F],
-                                                concurrentEffect: ConcurrentEffect[F], timer: Timer[F]): Stream[F, Unit] =
-      consumerStream(settings(groupId, broker))
-        .evalTap(_.subscribeTo(topic))
-        .flatMap {
-          _.stream
-            .flatMap(message => Stream.eval(
-              Logger[F].info(Avro.decode[A](message.record.value).toString)
-            ))
-        }
+  def stream[F[_], A: SchemaFor : FromRecord](groupId: String, topic: String, broker: String)
+                                             (implicit contextShift: ContextShift[F],
+                                              concurrentEffect: ConcurrentEffect[F], timer: Timer[F]): Stream[F, Unit] =
+    consumerStream(settings(groupId, broker))
+      .evalTap(_.subscribeTo(topic))
+      .flatMap {
+        _.stream
+          .flatMap(message => Stream.eval(
+            Logger[F].info(Avro.decode[A](message.record.value).toString)
+          ))
+      }
 }
